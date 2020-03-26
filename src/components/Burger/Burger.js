a024ee41d1f9d4d0e67bfcc5dc5ab56bb0d87e3b
josephtaylor1 <joetaylor2252>
2020-03-26 13:36:47 -0400

    makes burger.js dynamic

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,23 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => { //a wrapper around the burgerIngredient file
+    let transformIngredients = Object.keys(props.ingredients) //turns ingredients state from BurgerBuilder into an Array of keys
+    .map(igKey => { //maps each returned key i.e. ingredient use in the type prop
+        return [...Array(props.ingredients[igKey])] //creates an array with length equal to the listed amount of ingredients in the BurgerBuilder state
+            .map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} /> //this map returns a BurgerIngredient comp. based on the length of the array
+            }); //the i is the index given to the key prop
+    })
+    .reduce((arr, el) => {
+        return arr.concat(el);
+    }, []); //this will combine the spread arrays back into a single array
+    if (transformIngredients.length === 0) {
+        transformIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type='bread-top' />
-            <BurgerIngredient type='cheese' />
-            <BurgerIngredient type='meat' />
+            {transformIngredients}
             <BurgerIngredient type='bread-bottom' />
         </div>
     );
