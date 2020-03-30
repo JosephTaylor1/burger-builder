7eed8376389cd11afe039a931456b99ebaafb593
josephtaylor1 <joetaylor2252>
2020-03-30 12:51:58 -0400

    correct disabled button on More control

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -6,8 +6,7 @@ const buildControl = (props) => (
         <div className={classes.Label}>{props.label}</div>
         <button
             className={classes.More}
-            onClick={props.added}
-            disabled={props.disabled}>More</button>
+            onClick={props.added}>More</button>
         <button
             className={classes.Less}
             onClick={props.removed}
